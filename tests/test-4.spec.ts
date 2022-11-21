6e2c97d63daa1cefb52edf1ee08e6b72b3bf2710
TwisT3RHU <kgeri99@windowslive.com>
2022-11-21 16:06:32 +0100

    kis update

--- tests/test-4.spec.ts
+++ tests/test-4.spec.ts
@@ -1,10 +1,12 @@
 import { test, expect } from '@playwright/test';
 
-import { branch, login, medaurl } from './globalis';
+import { branch, login, medaurl, randomname } from './globalis';
 import { misc, user } from './core.json';
 
-const remotename = branch(true) + '_autoteszt';
-const servername = branch(false) + '_autoteszt';
+
+const randname = randomname("_autoteszt")
+const remotename = branch(true) + randname;
+const servername = branch(false) + randname;
 
 test.beforeEach(async ({ page }) => { // gyakorlatilag ez a precondition; legyen bejelentkezve
 
@@ -47,80 +49,30 @@ test('új távoli szerver hozzáadása', async ({ page }) => {
   await page.getByRole('button', { name: ' Mentés' }).click();
   await page2.getByRole('button', { name: ' Mentés' }).click();
 
+  await page.getByRole('button', { name: ' Távoli felhasználók' }).click();
+  await expect(page).toHaveURL(/.#!serverUsers./);
+  await page.getByRole('combobox').locator('div').click();
+  await page.locator('td[role="listitem"]:has-text("'+ remotename +'")').click();
+  await page.getByRole('cell', { name: user.name }).first().click();
+  await page.getByRole('button', { name: ' Hozzáad' }).first().click();
+  await page.getByRole('row', { name: remotename + " " + user.name }).getByRole('cell', { name: user.name }).click();
+  await page.getByRole('row', { name: user.usergroup }).getByLabel('').check();
+  await page.getByRole('button', { name: ' Hozzáad' }).nth(1).click();
+
+  await page2.getByRole('button', { name: ' Távoli felhasználók' }).click();
+  await expect(page2).toHaveURL(/.#!serverUsers./);
+  await page2.getByRole('combobox').locator('div').click();
+  await page2.locator('td[role="listitem"]:has-text("'+ servername +'")').click();
+  await page2.getByRole('cell', { name: user.name }).first().click();
+  await page2.getByRole('button', { name: ' Hozzáad' }).first().click();
+  await page2.getByRole('row', { name: servername + " " + user.name }).getByRole('cell', { name: user.name }).click();
+  await page2.getByRole('row', { name: user.usergroup }).getByLabel('').check();
+  await page2.getByRole('button', { name: ' Hozzáad' }).nth(1).click();
+
+
 });
-
+/*
 test.skip('test', async ({ page }) => {
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).click();
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).dblclick();
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).click();
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).click();
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).click({
-    clickCount: 5
-  });
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).press('Control+c');
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).fill('XgTtZugBs4JwnC32aSnxBYUueFcTZpet');
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).press('Control+a');
-
-  await page.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).press('Control+c');
-
-  await page1.goto('http://sso.beta.local/auth/realms/healthware/protocol/openid-connect/auth?response_type=code&client_id=medalyse3admin&redirect_uri=http%3A%2F%2Fmedalyse.beta.local%2Fapp%2Fmedalyse3admin%2F&state=64ba2134-49d1-408a-81d1-c734bc9548d7&login=true&scope=openid');
-
-  await page1.getByText('Jelszó').click();
-
-  await page1.getByLabel('Jelszó').click();
-
-  await page1.getByLabel('Jelszó').fill('123456');
-
-  await page1.goto('http://medalyse.beta.local/app/medalyse3admin/');
-
-  await page1.getByText('►Hozzáférések').click();
-
-  await page1.getByText('Szerverek').click();
-  await expect(page1).toHaveURL('http://medalyse.beta.local/app/medalyse3admin/#!servers');
-
-  await page1.getByRole('button', { name: ' Új' }).click();
-
-  await page1.getByRole('textbox', { name: 'Név' }).click();
-
-  await page1.getByRole('textbox', { name: 'Név' }).fill('gamma3');
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat URL' }).click();
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat URL' }).fill('http://medalyse.gamma.local/app/');
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat URL' }).press('Shift+ContextMenu');
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat URL' }).fill('http://medalyse.gamma.local/app/medalyse3app/call');
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).click();
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).press('Control+a');
-
-  await page1.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).fill('XgTtZugBs4JwnC32aSnxBYUueFcTZpet');
-
-  await page1.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).click();
-
-  await page1.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).press('Control+a');
-
-  await page1.getByRole('textbox', { name: 'Bejövő kapcsolat kód' }).press('Control+c');
-
-  await page.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).click();
-
-  await page.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).press('Control+a');
-
-  await page.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).fill('GUMJQjQrVaLyLsPCmjwhsqE1jLaSVnC9');
-
-  await page1.getByRole('button', { name: ' Mentés' }).click();
-
-  await page.getByRole('button', { name: ' Mentés' }).click();
 
   await page.getByRole('button', { name: ' Távoli felhasználók' }).click();
   await expect(page).toHaveURL('http://medalyse.gamma.local/app/medalyse3admin/#!serverUsers/1000000026');
@@ -221,4 +173,4 @@ test.skip('kapcsolatok', async ({ page }) => {
   const kimenokod = await page.getByRole('textbox', { name: 'Kimenő kapcsolat kód' }).inputValue();
   console.log(kimenokod + " KIMENŐ KÓD HELÓ VAN ITT VALAMI?");
 
-});
+});*/
