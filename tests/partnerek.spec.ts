02e77b6dab9c435f40d2d4f86821bb7c72d5ffe9
TwisT3RHU <kgeri99@windowslive.com>
2022-11-30 16:46:56 +0100

    expectekkel játszadozás

--- tests/partnerek.spec.ts
+++ tests/partnerek.spec.ts
@@ -21,14 +21,21 @@ test.afterEach(async ({ page }) => {
 
 test.describe.serial("egy partnert érintő tesztek", () => {
   test("partner hozzáadás", async ({ page }) => {
-    await page.getByRole("textbox", { name: "Név" }).click();
-    await page.getByRole("textbox", { name: "Név" }).fill(testname);
+    const textboxname = page.getByRole("textbox", { name: "Név" });
+    const cellname = page.getByRole("cell", { name: testname });
+    await expect(textboxname).toBeEditable();
+    await textboxname.click();
+    await expect(textboxname).toBeFocused();
+    await textboxname.fill(testname);
     await page.getByRole("button", { name: " Mentés" }).click();
+    await expect(cellname).toHaveText(testname);
     console.log(testname + " létrehozva");
   });
 
   test("partner törlés + megerősítő ablak", async ({ page }) => {
-    await page.getByRole("cell", { name: testname }).click();
+    const cellname = page.getByRole("cell", { name: testname });
+    await expect(cellname).toHaveText(testname);
+    await cellname.click();
     await page.getByRole("button", { name: " Törlés" }).click();
     await page.getByRole("button", { name: "Nem" }).click();
     await page.getByRole("button", { name: " Törlés" }).click();
@@ -38,7 +45,9 @@ test.describe.serial("egy partnert érintő tesztek", () => {
 
   test("partner visszaállítás", async ({ page }) => {
     await page.getByText("Töröltek").click();
-    await page.getByRole("cell", { name: testname }).click();
+    const cellname = page.getByRole("cell", { name: testname });
+    await expect(cellname).toHaveText(testname);
+    await cellname.click();
     await page.getByRole("button", { name: " Visszaállítás" }).click();
     await page.getByText("Töröltek").click();
     console.log(testname + " visszaállítva");
@@ -54,9 +63,11 @@ test.describe.serial("egy partnert érintő tesztek", () => {
   });
 
   test("partner ismételt törlése", async ({ page }) => {
-    await page.getByRole("cell", { name: testname }).click();
+    const cellname = page.getByRole("cell", { name: testname });
+    await expect(cellname).toHaveText(testname);
+    await cellname.click();
     await page.getByRole("button", { name: " Törlés" }).click();
-    await page.getByRole("button", { name: "Igen" }).click();
+    await page.getByRole("button", { name: "Igen" }).click(); // EZEKBŐL FUNKCIÓT CSINÁLNI, MERT LEHET! HOLNAP! 
     console.log(testname + " ismételten törölve");
   });
 });
