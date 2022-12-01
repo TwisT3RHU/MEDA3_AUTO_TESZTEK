bfceca75a0fc2dee9649f18d0ee0cc79910e917d
TwisT3RHU <kgeri99@windowslive.com>
2022-12-01 11:23:27 +0100

    login logout teszt

---
+++ tests/login-logout.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from "@playwright/test";
+
+import { login, medaurl } from "./globalis";
+
+test("szimpla login/logout", async ({ page }) => {
+    login(page);
+    await expect(page).toHaveURL(medaurl(false));
+    await page.locator('span:has-text("kilépés")').first().click();
+});
