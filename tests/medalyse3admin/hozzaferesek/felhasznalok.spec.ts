86c23c6b54bec20cd5b860ad82a0c3af1ee5bb21
TwisT3RHU <kgeri99@windowslive.com>
2023-01-10 10:45:40 +0100

    ez át lett írva

--- tests/medalyse3admin/hozzaferesek/felhasznalok.spec.ts
+++ tests/medalyse3admin/hozzaferesek/felhasznalok.spec.ts
@@ -106,43 +106,22 @@ test.describe.serial(misc.bulkcount + " felhasználót érintő tesztek", () => 
   test(misc.bulkcount + " felhasználó létrehozása", async ({ page }) => {
     for (let index = 1; index < misc.bulkcount + 1; index++) {
       const randomname2 = testname + "_" + index;
-      await page.getByRole("textbox", { name: "Név" }).fill(randomname2);
-      await page.getByRole("textbox", { name: "Név" }).press("Tab");
-      await page.getByRole("textbox", { name: "Jelszó" }).fill(randomname2);
-      await page.getByRole("textbox", { name: "Jelszó" }).press("Tab");
-      await page
-        .getByRole("textbox", { name: "Jelszó mégegyszer" })
-        .fill(randomname2);
-      await page
-        .getByRole("textbox", { name: "Teljes név" })
-        .fill("autoteszt user " + index);
-      await page.getByRole("textbox", { name: "Teljes név" }).press("Tab");
-      await page
-        .getByRole("textbox", { name: "Email cím" })
-        .fill("autoteszt@" + index + "example.com");
-      await page.getByRole("textbox", { name: "Email cím" }).press("Tab");
-      await page
-        .getByRole("textbox", { name: "Bejelentkező kód" })
-        .fill(randomname2);
+      const textboxes: string[][] = [
+        ["Név", "Jelszó", "Jelszó mégegyszer", "Teljes név", "Email cím", "Bejelentkező kód"],
+        [randomname2, randomname2, randomname2, "autoteszt user", "autoteszt" + index + "@example.com", randomname2]
+      ];
+      for (let i= 0; i < textboxes[0].length; i++) {
+        await textboxcheck(page, textboxes[0][i], textboxes[1][i]);
+      };
       //await page.getByRole('row', { name: 'B. kód érvényesség' }).locator('button').click();
       //await page.getByRole('button', { name: '»' }).click();
       //await page.getByRole('row', { name: '23 24 25 26 27 28 29' }).getByText('29').click();
-      await page
-        .getByRole("row", { name: "Partner" })
-        .getByRole("combobox")
-        .locator("div")
-        .click();
-      await page.getByText(user.partner).click();
-      await page
-        .getByRole("row", { name: "Alapértelmezett nyelv" })
-        .getByRole("combobox")
-        .locator("div")
-        .click();
-      await page.getByText("Magyar").click();
+      await rowcheck(page, "Partner", user.partner);
+      await rowcheck(page, "Alapértelmezett nyelv", "Magyar");
       await page.getByRole("button", { name: " Mentés" }).click();
       console.log(randomname2 + " létrehozva");
       await page.getByRole("cell", { name: randomname2 }).click();
-    }
+    };
   });
 
   test(misc.bulkcount + " felhasználó adatainak módosítása", async ({ page }) => {
@@ -150,41 +129,19 @@ test.describe.serial(misc.bulkcount + " felhasználót érintő tesztek", () => 
         const randomname2 = testname + "_" + index;
         await page.getByRole("cell", { name: randomname2 }).click();
         const randomname2edit = randomname2 + "_edited";
-        await page
-          .getByRole("textbox", { name: "Jelszó" })
-          .fill(randomname2edit);
-        await page.getByRole("textbox", { name: "Jelszó" }).press("Tab");
-        await page
-          .getByRole("textbox", { name: "Jelszó mégegyszer" })
-          .fill(randomname2edit);
-        await page
-          .getByRole("textbox", { name: "Teljes név" })
-          .fill("autoteszt user_edited" + index);
-        await page.getByRole("textbox", { name: "Teljes név" }).press("Tab");
-        await page
-          .getByRole("textbox", { name: "Email cím" })
-          .fill("autoteszt_edited@" + index + "example.com");
-        await page.getByRole("textbox", { name: "Email cím" }).press("Tab");
-        await page
-          .getByRole("textbox", { name: "Bejelentkező kód" })
-          .fill(randomname2edit);
-        //await page.getByRole('row', { name: 'B. kód érvényesség' }).locator('button').click();
-        //await page.getByRole('button', { name: '»' }).click();
-        //await page.getByRole('row', { name: '23 24 25 26 27 28 29' }).getByText('29').click();
-        //await page.getByRole('row', { name: 'Partner' }).getByRole('combobox').locator('div').click();
-        //await page.getByText(user.usergroup).click();
-        await page
-          .getByRole("row", { name: "Alapértelmezett nyelv" })
-          .getByRole("combobox")
-          .locator("div")
-          .click();
-        await page.getByText("English").click();
+        const textboxes: string[][] = [
+          ["Jelszó", "Jelszó mégegyszer", "Teljes név", "Email cím", "Bejelentkező kód"],
+          [randomname2edit, randomname2edit, randomname2edit, "autoteszt" + index + "_edited@example.com", randomname2edit]
+        ];
+        for (let i= 0; i < textboxes[0].length; i++) {
+          await textboxcheck(page, textboxes[0][i], textboxes[1][i]);
+        };
+        await rowcheck(page, "Alapértelmezett nyelv", "English");
         await page.getByRole("button", { name: " Mentés" }).click();
         console.log(randomname2edit + " módosítva");
         await page.getByRole("cell", { name: randomname2 }).click();
-      }
-    }
-  );
+      };
+  });
 
   test(misc.bulkcount + " felhasználó törlése", async ({ page }) => {
     for (let index = 1; index < misc.bulkcount + 1; index++) {
@@ -193,8 +150,9 @@ test.describe.serial(misc.bulkcount + " felhasználót érintő tesztek", () => 
       await page.getByRole("cell", { name: randomname2edit }).click();
       await removeitem(page, " Törlés");
       console.log(randomname2edit + " törölve");
-    }
+    };
   });
+  
   test.afterEach(async ({ page }) => {
     await logout(page);
   });
