719345add2002b6d8d1d3134b3171135a28d0686
TwisT3RHU <kgeri99@windowslive.com>
2023-01-30 16:45:12 +0100

    swap

--- tests/medalyse-ng-dev/general/darkmode.spec.ts
+++ tests/medalyse-ng-dev/general/darkmode.spec.ts
@@ -11,18 +11,18 @@ test.beforeEach(async ({ page }) => {
 test.describe.serial("témát érintő teszt", () => {
     test('sötét téma', async ({ page }) => {
         await page.locator('button:has-text("settings")').click();
-        const menuszin = page.locator('#mat-slide-toggle-2 > .mat-slide-toggle-label > .mat-slide-toggle-bar');
+        const sidebar = page.locator('#mat-slide-toggle-2 > .mat-slide-toggle-label > .mat-slide-toggle-bar');
 
-        await menuszin.click();
+        await sidebar.click();
         await setTimeout(1000);
-        const sidebar = page.locator('.mat-slide-toggle-bar').first();
-        let color = await sidebar.evaluate((e) => { return window.getComputedStyle(e).getPropertyValue("background-color") });
+        const menuszin = page.locator('.mat-slide-toggle-bar').first();
+        let color = await menuszin.evaluate((e) => { return window.getComputedStyle(e).getPropertyValue("background-color") });
         expect(color).toBe("rgb(27, 106, 61)");
         console.log(color); // dark szín
 
-        await menuszin.click();
+        await sidebar.click();
         await setTimeout(1000);
-        color = await sidebar.evaluate((e) => { return window.getComputedStyle(e).getPropertyValue("background-color") });
+        color = await menuszin.evaluate((e) => { return window.getComputedStyle(e).getPropertyValue("background-color") });
         expect(color).toBe("rgb(37, 85, 133)");
         console.log(color); // világos szín
 
