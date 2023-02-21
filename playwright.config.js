64ac882bf46bc134c32f6635459e2ba20a02a6a0
TwisT3RHU <kgeri99@windowslive.com>
2023-02-21 17:14:16 +0100

    pdf making de nem

--- playwright.config.js
+++ playwright.config.js
@@ -34,7 +34,12 @@ const config = {
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: [['html', { open: 'always' }]],
-  reporter: [['json', { outputFile: 'results.json' }]],
+  reporter: [
+    ['./src/custom-report.ts'],
+    ['list'],
+    ['html'],
+    ['json', { outputFile: 'results.json' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
---
+++ src/custom-report.ts
@@ -0,0 +1,39 @@
+import { PDFDocument, StandardFonts, rgb } from 'pdf-lib'
+import { Reporter } from '@playwright/test/reporter';
+
+async function createPdf() {
+    const pdfDoc = await PDFDocument.create()
+    const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+  
+    const page = pdfDoc.addPage()
+    const { width, height } = page.getSize()
+    const fontSize = 30
+    page.drawText('Creating PDFs in JavaScript is awesome!', {
+      x: 50,
+      y: height - 4 * fontSize,
+      size: fontSize,
+      font: timesRomanFont,
+      color: rgb(0, 0.53, 0.71),
+    })
+  
+    const pdfBytes = await pdfDoc.save()
+  }
+
+class MyReporter implements Reporter {
+  onBegin(config, suite) {
+    console.log(`Starting the run with ${suite.allTests().length} tests`);
+  }
+
+  onTestBegin(test) {
+    console.log(`Starting test ${test.title}`);
+  }
+
+  onTestEnd(test, result) {
+    console.log(`Finished test ${test.title}: ${result.status}`);
+  }
+
+  onEnd(result) {
+    console.log(`Finished the run: ${result.status}`);
+  }
+}
+export default MyReporter;
