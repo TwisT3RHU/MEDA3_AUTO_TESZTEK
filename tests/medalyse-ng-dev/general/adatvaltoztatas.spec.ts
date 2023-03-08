ac9b2c0657ac79472287c5695d39a013811f6378
TwisT3RHU <kgeri99@windowslive.com>
2023-03-08 14:45:58 +0100

    adatváltoztatás tweak

--- tests/medalyse-ng-dev/general/adatvaltoztatas.spec.ts
+++ tests/medalyse-ng-dev/general/adatvaltoztatas.spec.ts
@@ -19,31 +19,30 @@ test.describe.serial("adatmódosítást érintő tesztek", () => {
         email = email + "a";
         await page.getByLabel('Email *').fill(email);
         await testfunc.pressbutton(page, "Mentés");
+        await testfunc.pressbutton(page, "Rendben");
         console.log("e-mail módosítva");
     });
     test('nyelv módosítása', async ({ page }) => {
         await testfunc.pressbutton(page, "country", 0, "combobox");
         await page.getByText('English').click();
         await testfunc.pressbutton(page, "Mentés");
+        await testfunc.pressbutton(page, "Rendben");
         console.log("nyelv módosítva");
     });
     test('jelszó ellenőrzése, módosítása', async ({ page }) => {
-        await page.getByLabel('Régi jelszó *').fill(password + "b");
+        await page.getByLabel('Régi jelszó *').fill(password + "b"); // direkt rossz jelszó
         await page.locator('div').filter({ hasText: 'Új jelszó *' }).nth(4).click();
         await page.getByLabel('Új jelszó *').fill(password);
         await page.getByLabel('Új jelszó mégegyszer *').fill(password);
-
         await testfunc.pressbutton(page, "Jelszó változtatás");
-        await page.getByText('Could not modify user ['+ username +'] password! Current password is invalid!').click();
+        await page.getByText('Could not modify user ['+ username +'] password! Current password is invalid!').click(); 
         await testfunc.pressbutton(page, "Rendben");
-       
-        await page.getByLabel('Régi jelszó *').fill(password);
+        await page.getByLabel('Régi jelszó *').fill(password); // ez már rossz jelszó
         password = password + "a"; // újabb loginnál is ezt fogja bemásolni a teszt közben!
         await page.getByLabel('Új jelszó *').fill(password);
         await page.getByLabel('Új jelszó mégegyszer *').fill(password);
         await testfunc.pressbutton(page, "Jelszó változtatás");
         await page.getByText('Jelszó módosítva').click();
-
         await testfunc.pressbutton(page, "Rendben");
         console.log("jelszó módosítva");
     });
@@ -63,13 +62,14 @@ test.describe.serial("adatmódosítást érintő tesztek", () => {
         await page.getByLabel('Email *').click();
         await page.getByLabel('Email *').fill(email);
         await testfunc.pressbutton(page, "Mentés");
+        await testfunc.pressbutton(page, "Rendben");
         console.log("e-mail visszaállítva");
     });
     test('nyelv visszaállítása', async ({ page }) => {
         await testfunc.pressbutton(page, "country", 0, "combobox");
-        //await page.getByRole('combobox', { name: 'country', exact: true }).click();
         await page.getByText('Magyar').click();
         await testfunc.pressbutton(page, "Mentés");
+        await testfunc.pressbutton(page, "Rendben");
         console.log("nyelv visszaállítva");
     });
     test.afterEach(async ({ page }) => {
